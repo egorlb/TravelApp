e3a4514991a22dfc0f938bbf31f06e03031efb86
Egor Laba <labaegor1998@gmail.com>
2020-08-03 21:15:13 +0300

    commit created a project

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by Egor on 03/08/2020.
+//  Copyright © 2020 Egor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
